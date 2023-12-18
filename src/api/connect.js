69501447b8416935dcc7f41f7c7f2c652bce0e7a
scopetion <143470966+chrdeer@users.noreply.github.com>
2023-12-18 12:07:25 +0800

    update

--- src/api/connect.js
+++ src/api/connect.js
@@ -21,10 +21,10 @@ const connect = async (state, dispatch) => {
     }
     try {
         await getWeb3().then(async (result) => {
-            
-            dispatch({type:"CONNECT",payload:result.web3})
-            let chainID = await window.ethereum.request({methods:"eth_chainId"})
-            dispatch({type:"SET_NETWORK",payload:network})
+
+            dispatch({ type: "CONNECT", payload: result.web3 })
+            let chainID = await window.ethereum.request({ methods: "eth_chainId" })
+            dispatch({ type: "SET_NETWORK", payload: chainID })
 
             window.ethereum.request({ methods: 'eth_requestAccounts' }).then(async accounts => {
                 if (accounts && accounts.length > 0) {
@@ -33,21 +33,46 @@ const connect = async (state, dispatch) => {
                 }
             })
 
-            window.ethereum.on('accountsChanged',async (accounts)=>{
-                dispatch({type:"SET_ACCOUNT",payload:account})
-                let balance = await web3().
-            })
-
-            window.ethereum.on({ methods: "chainId" }).then((network) => {
+            window.ethereum.request({ methods: "eth_chainId" }).then((network) => {
                 dispatch({ type: "SET_NETWORK", payload: network })
 
             })
+
+            window.ethereum.on('accountsChanged', async (accounts) => {
+                dispatch({ type: "SET_ACCOUNT", payload: accounts })
+                result.web3().eth.getAccounts(accounts).then(async res => {
+                    let balance = await result.web3().eth.getBalance(res[0])
+                    dispatch({type:"SET_BALANCE",payload:balance / 10**18})
+                })
+            })
+
+            window.ethereum.on('chainChanged',()=>{
+                result.web3().eth.getAccounts().then(async res=>{
+                    let balance = await result.web3().eth.getBalance(res[0])
+                    dispatch({type:"SET_BALANCE",payload:balance / 10**18})
+                })
+            })
+
+        
         })
     }
     catch {
+        dispatch({type:"CONNECT_ERROR"})
 
     }
-
 }
 
-export { connect }
+const CreContext = React.createContext()
+const ConnectPro = async (props)=>{
+    const [apiState,actions] = useReducer(apiState,actions)
+
+    return(
+        <CreContext.Provider value={apiState}>
+            {props.children}
+        </CreContext.Provider>
+    )
+}
+
+const useCreContext = ()=>({...useContext(CreContext)})
+
+export { connect,CreContext,ConnectPro,useCreContext }
